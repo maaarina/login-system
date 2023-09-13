20b282b629e4171aaad535a373feeee66f2b06b4
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-09-13 18:03:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Initial page</title>
+</head>
+<body>
+    
+    <<?php 
+
+        if (!isset($_SESSION['login'])){
+
+            if (isset($_POST['enviar']) ){
+
+                $name = 'marina';
+                $password = '123456';
+
+                $nameForm = $_POST['name'];
+                $passwordForm = $_POST['password'];
+
+                if ($name == $nameForm && $password == $passwordForm) {
+
+                    $_SESSION['name'] = $name;
+                    header('Location: home.php');
+                    exit;
+                } else {
+                    echo 'Algum erro aconteceu!';
+                }
+            }
+
+            include('login.php');
+        } else {
+            include('home.php');
+        }
+
+    ?>
+
+</body>
+</html>
